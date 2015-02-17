3c97103c0dd871e8169e8a29419ab97971d2e68c
francois-durand <fradurandpub@gmail.com>
2015-02-17 14:42:02 +0100

    Add mathjax_path

--- docs/conf.py
+++ docs/conf.py
@@ -60,6 +60,8 @@ import svvamp
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.mathjax']
 
+mathjax_path = 'https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML'
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
