95518d27325dbc1f8e24cf03ccebab49aeeebb0f
francois-durand <fradurandpub@gmail.com>
2021-07-01 17:38:02 +0200

    Improve coverage

---
+++ tests/test_rule_range_voting.py
@@ -0,0 +1,103 @@
+import numpy as np
+from svvamp import GeneratorProfileLadder
+from svvamp.utils.misc import initialize_random_seeds
+from svvamp import RuleRangeVoting
+
+
+def test_min_grade_setter():
+    """
+        >>> rule = RuleRangeVoting()
+        >>> rule.min_grade = 0
+        >>> rule.min_grade
+        0
+        >>> rule.min_grade = -42
+        >>> rule.min_grade
+        -42
+        >>> rule.min_grade = -np.inf
+        Traceback (most recent call last):
+        ValueError: Unknown value for min_grade: -inf
+    """
+    pass
+
+
+def test_max_grade_setter():
+    """
+        >>> rule = RuleRangeVoting()
+        >>> rule.max_grade = 1
+        >>> rule.max_grade
+        1
+        >>> rule.max_grade = 42
+        >>> rule.max_grade
+        42
+        >>> rule.max_grade = np.inf
+        Traceback (most recent call last):
+        ValueError: Unknown value for max_grade: inf
+    """
+    pass
+
+
+def test_step_grade_setter():
+    """
+        >>> rule = RuleRangeVoting()
+        >>> rule.step_grade = 0
+        >>> rule.step_grade
+        0
+        >>> rule.step_grade = 0.1
+        >>> rule.step_grade
+        0.1
+        >>> rule.step_grade = np.inf
+        Traceback (most recent call last):
+        ValueError: Unknown value for step_grade: inf
+    """
+    pass
+
+
+def test_rescale_grades_setter():
+    """
+        >>> rule = RuleRangeVoting()
+        >>> rule.rescale_grades = True
+        >>> rule.rescale_grades
+        True
+        >>> rule.rescale_grades = False
+        >>> rule.rescale_grades
+        False
+        >>> rule.rescale_grades = 'unexpected value'
+        Traceback (most recent call last):
+        ValueError: Unknown value for rescale_grades: unexpected value
+    """
+    pass
+
+
+def test_no_rescale_grades():
+    """
+        >>> initialize_random_seeds()
+        >>> profile = GeneratorProfileLadder(n_v=5, n_c=3, n_rungs=5)().profile_
+        >>> rule = RuleRangeVoting(rescale_grades=False)(profile)
+        >>> rule.w_
+        2
+    """
+    pass
+
+
+def test_step_grade_not_equal_to_zero():
+    """
+        >>> initialize_random_seeds()
+        >>> profile = GeneratorProfileLadder(n_v=5, n_c=3, n_rungs=5)().profile_
+        >>> rule = RuleRangeVoting(step_grade=0.1)(profile)
+        >>> rule.w_
+        0
+    """
+    pass
+
+
+def test_tm_um_c():
+    """
+        >>> initialize_random_seeds()
+        >>> profile = GeneratorProfileLadder(n_v=5, n_c=3, n_rungs=5)().profile_
+        >>> rule = RuleRangeVoting()(profile)
+        >>> rule.is_tm_c_(0)
+        False
+        >>> rule.is_um_c_(0)
+        False
+    """
+    pass
