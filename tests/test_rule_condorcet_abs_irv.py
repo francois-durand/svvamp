d5eccbdf77e1ae4ac1a1b25aca56b728fb93dfb7
francois-durand <fradurandpub@gmail.com>
2021-07-02 17:41:54 +0200

    Improve coverage

---
+++ tests/test_rule_condorcet_abs_irv.py
@@ -0,0 +1,47 @@
+from svvamp.utils.misc import initialize_random_seeds
+from svvamp import RuleCondorcetAbsIRV, GeneratorProfileLadder
+
+
+def test_cm_slow():
+    """
+        >>> cm_option = 'slow'
+        >>> initialize_random_seeds()
+        >>> profile = GeneratorProfileLadder(n_v=5, n_c=3, n_rungs=5)().profile_
+        >>> rule = RuleCondorcetAbsIRV(cm_option=cm_option)(profile)
+        >>> rule.candidates_cm_
+        array([0., 0., 0.])
+    """
+    pass
+
+
+def test_cm_almost_exact():
+    """
+        >>> cm_option = 'almost_exact'
+        >>> initialize_random_seeds()
+        >>> profile = GeneratorProfileLadder(n_v=5, n_c=3, n_rungs=5)().profile_
+        >>> rule = RuleCondorcetAbsIRV(cm_option=cm_option)(profile)
+        >>> rule.candidates_cm_
+        array([0., 0., 0.])
+    """
+    pass
+
+
+def test_cm_exact():
+    """
+        >>> cm_option = 'exact'
+        >>> initialize_random_seeds()
+        >>> profile = GeneratorProfileLadder(n_v=5, n_c=3, n_rungs=5)().profile_
+        >>> rule = RuleCondorcetAbsIRV(cm_option=cm_option)(profile)
+        >>> rule.candidates_cm_
+        array([0., 0., 0.])
+    """
+    pass
+
+
+def test_cm__option_unexpected():
+    """
+        >>> rule = RuleCondorcetAbsIRV(cm_option='foo')()
+        Traceback (most recent call last):
+        ValueError: Unknown value for cm_option: foo
+    """
+    pass
