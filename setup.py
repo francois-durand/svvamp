7a038847b54668bc6e2cf730ee29c7c31433a1e7
francois-durand <fradurandpub@gmail.com>
2021-07-28 13:05:21 +0200

    Bump version: 0.6.8 → 0.6.9

--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/francois-durand/svvamp',
-    version='0.6.8',
+    version='0.6.9',
     zip_safe=False,
 )
